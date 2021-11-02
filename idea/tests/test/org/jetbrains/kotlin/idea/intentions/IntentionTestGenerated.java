JetBrains/intellij-community

[kotlin] Convert member to extension: do not preserve declaration-site variance on type parameters
#KTIJ-6817 Fixed

plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
@@ -9260,6 +9260,11 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
                 runTest("testData/intentions/declarations/convertMemberToExtension/genericClass.kt");
             }
 
+            @TestMetadata("genericClassWithVariance.kt")
+            public void testGenericClassWithVariance() throws Exception {
+                runTest("testData/intentions/declarations/convertMemberToExtension/genericClassWithVariance.kt");
+            }
+
             @TestMetadata("genericFun.kt")
             public void testGenericFun() throws Exception {
                 runTest("testData/intentions/declarations/convertMemberToExtension/genericFun.kt");

consulo/consulo-kotlin

closes https://github.com/JetBrains/intellij-community/pull/1794
(cherry picked from commit cc8eefef7f88ba5ba6cd0cafde68cd899c78d633)
IJ-MR-16355
GitOrigin-RevId: fc528efd3f834e3fc9dba3bf7c69dea7cf0d890c

idea/tests/test/org/jetbrains/kotlin/idea/intentions/IntentionTestGenerated.java
@@ -9290,6 +9290,21 @@ public abstract class IntentionTestGenerated extends AbstractIntentionTest {
                 runTest("testData/intentions/declarations/convertMemberToExtension/genericClass.kt");
             }
 
+            @TestMetadata("genericClassWithInVariance.kt")
+            public void testGenericClassWithInVariance() throws Exception {
+                runTest("testData/intentions/declarations/convertMemberToExtension/genericClassWithInVariance.kt");
+            }
+
+            @TestMetadata("genericClassWithOutVariance.kt")
+            public void testGenericClassWithOutVariance() throws Exception {
+                runTest("testData/intentions/declarations/convertMemberToExtension/genericClassWithOutVariance.kt");
+            }
+
+            @TestMetadata("genericClassWithVarianceAndReified.kt")
+            public void testGenericClassWithVarianceAndReified() throws Exception {
+                runTest("testData/intentions/declarations/convertMemberToExtension/genericClassWithVarianceAndReified.kt");
+            }
+
             @TestMetadata("genericFun.kt")
             public void testGenericFun() throws Exception {
                 runTest("testData/intentions/declarations/convertMemberToExtension/genericFun.kt");