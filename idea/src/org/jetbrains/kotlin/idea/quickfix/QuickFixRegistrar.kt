JetBrains/intellij-community

[kotlin] Register a quick fix for `SAFE_CALL_WILL_CHANGE_NULLABILITY`
The "Replace with dot call" (`ReplaceWithDotCallFix`) is
proposed (the same as for `UNNECESSARY_SAFE_CALL`).

^KTIJ-19985 Fixed

GitOrigin-RevId: d7877af5516faf620b7f1429d43e36740f8aeadb

plugins/kotlin/idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -241,6 +241,7 @@ class QuickFixRegistrar : QuickFixContributor {
 
         UNNECESSARY_SAFE_CALL.registerFactory(ReplaceWithDotCallFixFactory)
         UNSAFE_CALL.registerFactory(ReplaceWithSafeCallFixFactory)
+        SAFE_CALL_WILL_CHANGE_NULLABILITY.registerFactory(ReplaceWithDotCallFixFactory)
 
         UNSAFE_CALL.registerFactory(SurroundWithNullCheckFix)
         UNSAFE_IMPLICIT_INVOKE_CALL.registerFactory(SurroundWithNullCheckFix)

consulo/consulo-kotlin

(cherry picked from commit d7877af5516faf620b7f1429d43e36740f8aeadb)
GitOrigin-RevId: 1b3c04dd2a22682a9f7029ed115fe0d7c9e1c8b8

idea/src/org/jetbrains/kotlin/idea/quickfix/QuickFixRegistrar.kt
@@ -244,6 +244,7 @@ class QuickFixRegistrar : QuickFixContributor {
 
         UNNECESSARY_SAFE_CALL.registerFactory(Fe10ReplaceWithDotCallFixFactory)
         UNSAFE_CALL.registerFactory(ReplaceWithSafeCallFixFactory)
+        SAFE_CALL_WILL_CHANGE_NULLABILITY.registerFactory(Fe10ReplaceWithDotCallFixFactory)
 
         UNSAFE_CALL.registerFactory(SurroundWithNullCheckFix)
         UNSAFE_IMPLICIT_INVOKE_CALL.registerFactory(SurroundWithNullCheckFix)