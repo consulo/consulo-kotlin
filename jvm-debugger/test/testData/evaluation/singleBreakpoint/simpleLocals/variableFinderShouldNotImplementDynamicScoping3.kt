JetBrains/intellij-community

[KTIJ-19314] Debugger: Account for synthetic names of captured locals

plugins/kotlin/jvm-debugger/test/testData/evaluation/singleBreakpoint/simpleLocals/variableFinderShouldNotImplementDynamicScoping3.kt
@@ -1,0 +1,28 @@
+package variableFinderShouldNotImplementDynamicScoping2
+
+fun outer(x: Int): () -> Int {
+    fun inner(): Int {
+        return x
+    }
+    fun returned(): Int {
+        //Breakpoint!
+        return 0
+    }
+    return ::returned
+}
+
+fun test(x: Int) {
+    fun inner(x: Int) {
+        val y = x
+        val f = outer(54)
+        f()
+    }
+    inner(x)
+}
+
+fun main() {
+   test(45)
+}
+
+// EXPRESSION: x
+// RESULT: Cannot find local variable 'x' with type int

plugins/kotlin/jvm-debugger/test/testData/evaluation/singleBreakpoint/simpleLocals/variableFinderShouldNotImplementDynamicScoping4.kt
@@ -1,0 +1,28 @@
+package variableFinderShouldNotImplementDynamicScoping2
+
+fun outer(x: Int): () -> Int {
+    fun inner(): Int {
+        return x
+    }
+    fun returned(): Int {
+        //Breakpoint!
+        return 0
+    }
+    return ::returned
+}
+
+fun test(x: Int) {
+    fun inner() {
+        val y = x
+        val f = outer(54)
+        f()
+    }
+    inner()
+}
+
+fun main() {
+   test(45)
+}
+
+// EXPRESSION: x
+// RESULT: Cannot find local variable 'x' with type int

plugins/kotlin/jvm-debugger/test/testData/evaluation/singleBreakpoint/simpleLocals/variableFinderShouldNotImplementDynamicScoping5.kt
@@ -1,0 +1,19 @@
+package variableFinderShouldNotImplementDynamicScoping2
+
+fun outer(x: Int): () -> Int {
+    fun inner(): Int {
+        val y = x
+        val x = 57
+        //Breakpoint!
+        return x
+    }
+    return ::inner
+}
+
+
+fun main() {
+    outer(75)()
+}
+
+// EXPRESSION: x
+// RESULT: 57: I

consulo/consulo-kotlin

closes https://github.com/JetBrains/intellij-community/pull/2149
(cherry picked from commit ebeb8cce5d90a7ea0733edefc1712ef8eccc095d)
IJ-MR-95422
GitOrigin-RevId: a6c4f0c843ecb3e20e207fffbff5867257c321f0

jvm-debugger/test/testData/evaluation/singleBreakpoint/simpleLocals/variableFinderShouldNotImplementDynamicScoping3.kt
@@ -1,0 +1,28 @@
+package variableFinderShouldNotImplementDynamicScoping3
+
+fun outer(x: Int): () -> Int {
+    fun inner(): Int {
+        return x
+    }
+    fun returned(): Int {
+        //Breakpoint!
+        return 0
+    }
+    return ::returned
+}
+
+fun test(x: Int) {
+    fun inner(x: Int) {
+        val y = x
+        val f = outer(54)
+        f()
+    }
+    inner(x)
+}
+
+fun main() {
+   test(45)
+}
+
+// EXPRESSION: x
+// RESULT: Cannot find local variable 'x' with type int

jvm-debugger/test/testData/evaluation/singleBreakpoint/simpleLocals/variableFinderShouldNotImplementDynamicScoping4.kt
@@ -1,0 +1,28 @@
+package variableFinderShouldNotImplementDynamicScoping4
+
+fun outer(x: Int): () -> Int {
+    fun inner(): Int {
+        return x
+    }
+    fun returned(): Int {
+        //Breakpoint!
+        return 0
+    }
+    return ::returned
+}
+
+fun test(x: Int) {
+    fun inner() {
+        val y = x
+        val f = outer(54)
+        f()
+    }
+    inner()
+}
+
+fun main() {
+   test(45)
+}
+
+// EXPRESSION: x
+// RESULT: Cannot find local variable 'x' with type int

jvm-debugger/test/testData/evaluation/singleBreakpoint/simpleLocals/variableFinderShouldNotImplementDynamicScoping5.kt
@@ -1,0 +1,19 @@
+package variableFinderShouldNotImplementDynamicScoping5
+
+fun outer(x: Int): () -> Int {
+    fun inner(): Int {
+        val y = x
+        val x = 57
+        //Breakpoint!
+        return x
+    }
+    return ::inner
+}
+
+
+fun main() {
+    outer(75)()
+}
+
+// EXPRESSION: x
+// RESULT: 57: I