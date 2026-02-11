d549b0305cdabfd0c7581f7186a2ced7dd814041
Anna Kozlova <anna.kozlova@jetbrains.com>
2026-02-11 12:11:25 +0100

    [kotlin] extract K1ModeProjectStructureApi specific api to fe module; replace with AA the rest
    - intellij.kotlin.base.analysis.tests should be marked as k1 specific or migrated to test k2
    - currently not migrated:
       - android modules
       - intellij.kotlin.gradle.codeInsight.common
       - intellij.amper.syncTests
    
    KTIJ-37535
    
    GitOrigin-RevId: bb828362ac4f84b6f1ae92fc14e8c10988723dfc

--- base/project-structure/src/org/jetbrains/kotlin/idea/vfilefinder/IdeVirtualFileFinderFactory.kt
+++ base/project-structure/src/org/jetbrains/kotlin/idea/vfilefinder/IdeVirtualFileFinderFactory.kt
@@ -4,11 +4,7 @@ package org.jetbrains.kotlin.idea.vfilefinder
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
-import org.jetbrains.kotlin.analysis.api.KaImplementationDetail
-import org.jetbrains.kotlin.analysis.api.platform.projectStructure.KaResolutionScopeProvider
 import org.jetbrains.kotlin.analyzer.ModuleInfo
-import org.jetbrains.kotlin.idea.base.projectStructure.moduleInfo.IdeaModuleInfo
-import org.jetbrains.kotlin.idea.base.projectStructure.toKaModule
 import org.jetbrains.kotlin.load.kotlin.MetadataFinderFactory
 import org.jetbrains.kotlin.load.kotlin.VirtualFileFinder
 import org.jetbrains.kotlin.load.kotlin.VirtualFileFinderFactory
@@ -16,14 +12,8 @@ import org.jetbrains.kotlin.load.kotlin.VirtualFileFinderFactory
 class IdeVirtualFileFinderFactory(private val project: Project) : VirtualFileFinderFactory, MetadataFinderFactory {
     override fun create(scope: GlobalSearchScope): VirtualFileFinder = IdeVirtualFileFinder(scope, project)
 
-    @OptIn(KaImplementationDetail::class)
     override fun create(project: Project, module: ModuleInfo): VirtualFileFinder {
-        val scope = if (module is IdeaModuleInfo) {
-            KaResolutionScopeProvider.getInstance(project).getResolutionScope(module.toKaModule())
-        } else {
-            GlobalSearchScope.allScope(project)
-        }
-
-        return IdeVirtualFileFinder(scope, project)
+        // never called
+        return IdeVirtualFileFinder(GlobalSearchScope.allScope(project), project)
     }
 }
